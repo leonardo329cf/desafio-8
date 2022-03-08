df58f2d8cd04f0800e4839bd6126b08ad5ae66a4
leonardo ferreira <leonardo.ferreira@objective.com.br>
2022-03-08 17:19:39 -0300

    Project created

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './assets/styles/custom.scss';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+    <h1>Hello</h1>
+    <h2>Hello</h2>
+    </>
+
+  );
+}
+
+export default App;
